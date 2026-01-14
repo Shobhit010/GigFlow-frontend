ad4e81ac8b27b2f3fe571bdf3084b88e88c28a6f
Shobhit010 <shobhitpoddar410@gmail.com>
2026-01-14 15:50:00 +0530

    api update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseUrl = import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`
+    : '/api';
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || '/',
+    baseURL: baseUrl,
     headers: {
         'Content-Type': 'application/json',
     },
